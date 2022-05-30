edc891cd8a26a1d2eb915a277368ebb09cbb5616
Lucas Evangelista <lucas.silva@sptech.school>
2022-05-30 19:16:17 -0300

    Algumas 12 acrescentadas

--- bd/script_bd_cspedia.sql
+++ bd/script_bd_cspedia.sql
@@ -6,5 +6,4 @@ create table usuarios (idUsuario int primary key auto_increment,
 nick_name varchar(30),
 email varchar(50),
 senha varchar(30),
-fkIndicacao int, foreign key (fkIndicacao) references usuarios (idUsuario),
-indicado_por varchar(30));
+fkIndicacao varchar(30), foreign key (fkIndicacao) references usuarios (nick_name));
