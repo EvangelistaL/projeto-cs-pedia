335e79a9fc0ab212e215ddb5022eea4cf861962b
Lucas Evangelista <lucas.silva@sptech.school>
2022-06-06 22:00:26 -0300

    Script final do BD

--- bd/script_bd_cspedia.sql
+++ bd/script_bd_cspedia.sql
@@ -9,6 +9,19 @@ senha varchar(30),
 fkIndicacao varchar(30), foreign key (fkIndicacao) references usuarios (nick_name),
 primary key(nick_name));
 
-select * from usuarios;
+create table times (idTime int auto_increment primary key,
+nome_time varchar(30));
 
-select count(fkIndicacao) from usuarios where fkIndicacao = 'hariken';
+insert into times values (1, 'Furia'),
+(2, 'Imperial'),
+(3, 'paiN'),
+(4, 'MIBR'),
+(5, 'GODSENT');
+
+create table voto_times (fkUsuario int, foreign key (fkUsuario) references usuarios (idUsuario),
+fkTime int, foreign key (fkTime) references times (idTime),
+voto int);
+
+select * from voto_times;
+
+select fkTime, sum(voto) as 'votos' from voto_times group by fkTime order by fkTime;
